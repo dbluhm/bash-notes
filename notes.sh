f8a81e635c8ab48db56cb998767c7b1808f5f8a5
Daniel Bluhm <bluhmdan@gmail.com>
2017-07-11 11:55:03 -0600

    Fix weird notes.sh copy issue

--- notes.sh
+++ notes.sh
@@ -1,21 +1,104 @@
+#!/bin/bash
 NOTESDIR="$HOME/.notes"
 
-_notes () {
-    local cur
+if [ ! -d "$NOTESDIR" ]; then
+    mkdir $NOTESDIR
+    if [ ! $? -eq 0 ]; then
+        echo "Couldn't create $NOTESDIR"
+        exit 1
+    fi
+fi
 
-    COMPREPLY=()
-    cur=${COMP_WORDS[COMP_CWORD]}
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
+__usage() {
+    echo $1
+    echo "Usage text here"
+    exit 1;
+}
 
-    if [ "$prev" == "notes" ]; then
-        COMPREPLY=( $(compgen -W "--search -s --remove -r --add -a $(find $NOTESDIR -type d -name '.git' -prune -o -type d -not -name '.notes*' -not -name '.git' -printf "%f\n")" -- ${cur}))
-    elif [ "$prev" == "--search" ] || [ "$prev" == "-s" ] || [ "$prev" == "-r" ] || [ "$prev" == "--remove" ]; then
-        COMPREPLY=( $(compgen -W "$(find $NOTESDIR -type d -name '.git' -prune -o -type d -not -name '.notes*' -not -name '.git' -printf "%f\n")" -- ${cur}))
-    elif [ ${#COMP_WORDS[@]} -lt 5 ] && [ ! "$prev" == "--add" ] && [ ! "$prev" == "-a" ]; then
-        notebook=$(find $NOTESDIR -type d -name $prev)
-        COMPREPLY=( $(compgen -W "$(find $notebook -maxdepth 1 -type f -printf "%f\n" | sed -e 's/\.md$//')" -- ${cur}))
+__edit() {
+    path_to_notebook=$(find $NOTESDIR -type d -name $1) #Get full path
+    if [ -z $2 ]; then
+        echo "Opening new note in $1"
+        edit=""
+    else
+        echo "Opening $2.md from notebook $path_to_notebook"
+        edit="$path_to_notebook/$2.md"
+    fi
+
+    if hash nvim >/dev/null 2>&1; then
+        nvim "+cd $path_to_notebook" "+Goyo" "$edit"
+    elif hash $EDITOR >/dev/null 2>&1; then
+        old=$(pwd)
+        cd $path_to_notebook && $EDITOR "$edit"; cd $old
+    else
+        echo "Error: Your EDITOR environment variable is not configured!"
+        exit 1
     fi
 }
-complete -o nosort -F _notes notes
 
-# vi:syntax=sh
+__search() {
+    if hash ag  >/dev/null 2>&1; then
+        ag $1 $2
+    else
+        grep --color=auto --exclude-dir .git -iR $1 $2
+    fi
+}
+
+__remove() {
+    if [ -z $2 ]; then
+        echo "Warning: Deleting notebook $1. This will remove all notes within this notebook."
+        path_to_delete=$1
+    else
+        echo "Warning: Deleting note $2.md from notebook $1. You will not be able to recover this note easily."
+        path_to_delete="$1/$2.md"
+    fi
+    read -p "Are you sure? [y/n] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[yY]$ ]]; then
+        rm -rf $NOTESDIR/$path_to_delete
+        if [ $? -eq 0 ]; then echo "$path_to_delete removed."; else echo "Error while deleting $path_to_delete"; fi
+    else
+        echo "Cancelled"
+    fi
+    exit 0;
+}
+
+if [[ $# -gt 1 ]]; then
+    case $1 in
+        -s|--search)
+            SEARCHSTR="$2"
+            ;;
+        -r|--remove)
+            NOTEBOOKREMOVEPATH="$2"
+            NOTEREMOVEPATH="$3"
+            ;;
+        -a|--add)
+            ADDNAME="$2"
+            ;;
+        *)
+            NOTEBOOK="$1"
+            NOTE="$2"
+            shift
+            ;;
+    esac
+else
+    __usage "Invalid number of parameters"
+fi
+
+if [ -n "$SEARCHSTR" ]; then
+    echo "Searching all notesbooks for $SEARCHSTR..."
+    __search $SEARCHSTR $NOTESDIR
+    exit 0;
+elif [ -n "$NOTEBOOKREMOVEPATH" ]; then
+    __remove $NOTEBOOKREMOVEPATH $NOTEREMOVEPATH
+elif [ -n "$ADDNAME" ]; then
+    echo "Creating new notebook $ADDNAME..."
+    mkdir $NOTESDIR/$ADDNAME
+    if [ -d $NOTESDIR/$ADDNAME ]; then
+        echo "Notebook $ADDNAME successfully created"
+    else
+        echo "Error while creating Notbook $ADDNAME"
+    fi
+elif [ -n "$NOTEBOOK" ]; then
+    __edit $NOTEBOOK $NOTE #If note not set, create a new one
+fi
