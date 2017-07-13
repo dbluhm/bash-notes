852019fb6466023e2734f8435fe8ef61cc826c40
Daniel Bluhm <bluhmdan@gmail.com>
2017-07-13 14:30:42 -0600

    Check for changes before attempting to commit

--- notes-save.sh
+++ notes-save.sh
@@ -4,6 +4,9 @@ OLD_DIR=`pwd`
 cd $NOTESDIR
 git add .
 modified=$(git diff --name-status HEAD)
+if [ -z "$modified" ]; then
+    echo "Nothing to commit"
+fi
 count=$(wc -l <<< "$modified")
 git commit -m "$count file(s) changed
 
