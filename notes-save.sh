7482a972a1a7a992451ca3788d71fbb2b4b6a13a
Daniel Bluhm <bluhmdan@gmail.com>
2017-07-13 13:24:23 -0600

    Add a saving script for the notes

---
+++ notes-save.sh
@@ -0,0 +1,20 @@
+NOTESDIR="$HOME/.notes"
+
+OLD_DIR=`pwd`
+cd $NOTESDIR
+git add .
+modified=$(git diff --name-status HEAD)
+count=$(wc -l <<< "$modified")
+git commit -m "$count file(s) changed
+
+$modified"
+
+read -p "Push? [y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[yY]$ ]]; then
+    git push origin
+else
+    echo "Not pushing changes"
+fi
+
+cd $OLD_DIR
